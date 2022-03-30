ad05744567313146799d7dab2554cc7589d9853e
jamesnathanan <57458881+jamesnathanan@users.noreply.github.com>
2022-03-30 08:55:18 +0700

    Update removeDash.rb

--- removeDash.rb
+++ removeDash.rb
@@ -1,5 +1,6 @@
 Dir["./*"].each do |oldfile|
-  newName = oldfile.gsub("_","")
+  newName = oldfile.sub("_","")
+  newName = oldfile.sub(" ","")
   File.rename(oldfile, newName)
   puts "File #{oldfile} has been renamed!"
 end
